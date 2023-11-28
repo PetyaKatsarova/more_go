74fae50e97f0c470b397e5413ccdb3494e7df8ed
Katsarova <petya.katsarova@cgi.com>
2023-11-28 13:05:49 +0100

    work in progress

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module app
 
 go 1.21.4
+
+// require github.com/go-chi/chi/v5 v5.0.10 // indirect
+
+require github.com/go-chi/chi/v5 v5.0.10
---
+++ pr5_orders-api/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	// "context"
+	"fmt"
+	"context"
+	"github.com/PetyaKatsarova/more_go/pr5_orders-api/application"
+)
+
+func main() {
+	
+}
+
+// go mod tidy       
+// curl -X POST localhost:3000/hello -v         
